7bc1a7b4feba29423e6e85dd26c02156c44c1606
narwa <narwandaru.nara@gmail.com>
2024-11-14 00:18:44 +0700

    feat: JWK source with RSA key encoder and JWT decoder

--- src/main/java/dev/vianneynara/authorizationserver/configuration/SecurityConfig.java
+++ src/main/java/dev/vianneynara/authorizationserver/configuration/SecurityConfig.java
@@ -110,4 +110,39 @@ public class SecurityConfig {
 
 		return new InMemoryRegisteredClientRepository(oidcClient);
 	}
+
+	@Bean
+	public JWKSource<SecurityContext> jwkSource() {
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+			.privateKey(privateKey)
+			.keyID(UUID.randomUUID().toString())
+			.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	private static KeyPair generateRsaKey() {
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		} catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+
+	@Bean
+	public AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
 }
